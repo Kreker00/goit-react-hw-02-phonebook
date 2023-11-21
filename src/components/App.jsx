d6a7255236ae948a4ce939970d54a7f8081fa478
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-11-21 16:51:53 +0200

    djy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { GlobalStyle } from './GlobalStyle';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  // return (
+  //
+  // );
+}
